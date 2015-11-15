5d5834b7036ca6a0e52f72c75d5f5b838cfd8cd7
Nick Mai <nick.mai@outlook.com>
2015-11-15 11:15:33 -0800

    Fixed static file server

--- src/mithical.js
+++ src/mithical.js
@@ -1,13 +1,15 @@
 'use strict'
 
 let ejs = require('ejs')
+let __parent = parentDir(__dirname)
 let app
 ejs.delimiter = '%'
 
+// These strings will be replaced with `include('file.ejs')` later
 let head = '<head><script src="/mithril.min.js"></script><script src="/progressbar.js"></script></head>'
 let part_header = '<div>Hello, this is the header</div>'
 let part_footer = '<script src="/mithical.js"></script>'
-let part_body = '<style>#container: {width: 300px;height: 150px;}</style>'
+let part_body = ''
 let template_body = '<%- head -%><%- header -%><%- body -%><%- footer -%>'
 
 module.exports = function (express_app) {
@@ -18,12 +20,45 @@ module.exports = function (express_app) {
         {head: head, header: part_header, body: part_body, footer: part_footer}))
     })
     app.get('/mithril.min.js', function (req, res) {
-        res.sendFile(__dirname + '/../node_modules/mithril/mithril.min.js')
+        res.sendFile(__parent + '/node_modules/mithril/mithril.min.js')
     })
     app.get('/progressbar.js', function (req, res) {
-        res.sendFile(__dirname + '/../lib_dev/progressbar.js')
+        res.sendFile(__parent + '/lib_dev/progressbar.js')
     })
     app.get('/mithical.js', function (req, res) {
-        res.sendFile(__dirname + '/client/script.js')
+        res.sendFile(__parent + '/src/client/script.js')
     })
+}
+
+// I don't know why the hell this is necessary but I have no internet right now.
+// Simply using `__dirName + '/../asdf'` fails. Console output says FORBIDDEN
+function parentDir (dirname) {
+    // Parent dir will be an empty string unless '/' can be found
+    let pDir = ''
+    if (dirname) {
+        let i = dirname.lastIndexOf('/')
+        if (i < 0) {
+            console.log('1')
+            return pDir
+        }
+        pDir = dirname
+        if (i >= dirname.length - 1) {
+            if (dirname.length === 1) {
+                return '/'
+            } else {
+                pDir = pDir.substring(0, i)
+            }
+        } 
+        pDir = pDir.substring(0, i)
+        i = dirname.lastIndexOf('/')
+        if (i < 0) {
+            // You suck.
+            return ''
+        } else {
+            // Ah yes, finally.
+            return pDir.substring(0, i)
+        }
+    } else {
+        return pDir
+    }
 }
