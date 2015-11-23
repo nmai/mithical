f980fc7a6e5db6941109c1afb3d63828b7c274c3
Nick Mai <nick.mai@outlook.com>
2015-11-22 23:31:07 -0800

    added view model to todoslist

--- src/client/script.js
+++ src/client/script.js
@@ -30,18 +30,62 @@ let Todo = function(data) {
 }
 
 let TodoList = {
+    vm: (function() {
+        var vm = {}
+        vm.init = function() {
+            //a running list of todos
+            vm.list = new Array();
+
+            //a slot to store the name of a new todo before it is created
+            vm.description = m.prop('');
+
+            //adds a todo to the list, and clears the description field for user convenience
+            vm.add = function() {
+                if (vm.description()) {
+                    vm.list.push(new Todo({description: vm.description()}));
+                    vm.description('');
+                }
+            };
+
+            vm.checkOff = function(task) {
+                console.log(task.description())
+                // @TODO: optimize further and (maybe) break this out to a separate helper function
+                //        Or perhaps dispatch a custom event.
+                // Update the progress bar.
+                var complete = 0
+                vm.list.map( (t) => {
+                    console.log(t)
+                    if (t.done()) {
+                        ++complete
+                    }
+                })
+
+                // don't see how it could possibly be zero and trigger a check event,
+                // but it never hurts to play it safe
+                var le = vm.list.length
+                if (le > 0) {
+                    progressBar.animate(complete / le)
+                } else if (complete === le) {
+                    progressBar.animate(0)
+                } else {
+                    progressBar.set(0)
+                    throw new Error("Huh, looks like your list broke")
+                }
+
+                return {onclick: m.withAttr('checked', task.done), checked: task.done()}
+            }
+        }
+        return vm
+    }()),
     controller: function (args){
-
-    },
-    vm: {
-
+        TodoList.vm.init()
     },
     view: function (ctrl){
         m('div', {id: 'progress-visual', style: {maxWidth: '400px', marginBottom: '12px'} }),
-        m('input', {onchange: m.withAttr('value', vm.description), onkeypress: testfunc, value: todo.vm.description()}),
-        m('button', {onclick: todo.vm.add}, 'Add'),
+        m('input', {onchange: m.withAttr('value', this.vm.description), value: this.vm.description()}),
+        m('button', {onclick: this.vm.add}, 'Add'),
         m('table', [
-            todo.vm.list.map(function(task, index) {
+            this.vm.list.map(function(task, index) {
                 return m('tr', [
                     m('td', [
                         m('input[type=checkbox]', vm.checkOff(task))
@@ -84,7 +128,7 @@ let TodoList = {
 
 //initialize the application
 //m.mount(document, Container)
-m.mount(document.getElementById('container'), TodoList);
+m.mount(document, TodoList);
 
 var opts = {
     strokeWidth: 12.0,
